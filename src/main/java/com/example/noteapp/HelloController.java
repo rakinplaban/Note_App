2bfb5775b3a9af29975f4dc701d26b128defbdc1
rakinplaban <rakinshahriar54@gmail.com>
2023-06-11 15:21:21 +0600

    Register page.

--- src/main/java/com/example/noteapp/HelloController.java
+++ src/main/java/com/example/noteapp/HelloController.java
@@ -1,14 +1,33 @@
 package com.example.noteapp;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Button registerAcc;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField username;
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private PasswordField confimrpassword;
+
+
+    @FXML
+    protected void onRegisterButton() {
+        if(username.getText().isBlank()==false && email.getText().isBlank()==false && password.getText().isBlank()==false){
+
+        }
     }
 }
