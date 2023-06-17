a574d029fdc679a4386a26747211632df99d2832
rakinplaban <rakinshahriar54@gmail.com>
2023-06-17 22:25:07 +0600

    Successfully completed view categories.

--- src/main/java/com/example/noteapp/Categories.java
+++ src/main/java/com/example/noteapp/Categories.java
@@ -3,18 +3,38 @@ package com.example.noteapp;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.control.MenuBar;
 import javafx.stage.Stage;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.io.IOException;
 
 public class Categories {
+    private int id;
+    private String category_name;
+    private Date created_date;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCategory_name() {
+        return category_name;
+    }
+
+    public Date getCreated_date() {
+        return created_date;
+    }
+
     @FXML
     private Stage primaryStage; // Declare the Stage reference
 
@@ -22,11 +42,26 @@ public class Categories {
         this.primaryStage = primaryStage;
     }
 
+    public Categories(){}
+
+    public Categories(int id, String category_name, Date created_date) {
+        this.id = id;
+        this.category_name = category_name;
+        this.created_date = created_date;
+    }
+
+
     @FXML
     private TextField categoryid;
 
     @FXML
     private Button createbtn;
+
+    @FXML
+    private Menu view;
+
+    @FXML
+    private MenuBar menubar;
 
     @FXML
     public void createCategory(){
@@ -49,6 +84,22 @@ public class Categories {
         }
     }
 
+    @FXML
+    public void showCategories(){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("categorylist.fxml"));
+            AnchorPane categoryLayout = loader.load();
+            Scene loginScene = new Scene(categoryLayout);
+
+            // Access the controller of the login.fxml file and pass the primaryStage reference
+            ViewCategories categoriesController = loader.getController();
+            categoriesController.setPrimaryStage(primaryStage);
+
+            primaryStage.setScene(loginScene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     private void showAlert(String title, String message) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
--- src/main/java/com/example/noteapp/ViewCategories.java
+++ src/main/java/com/example/noteapp/ViewCategories.java
@@ -1,57 +1,40 @@
 package com.example.noteapp;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.Menu;
-import java.io.IOException;
+import javafx.stage.Stage;
 import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Date;
 
 public class ViewCategories {
+    private Stage primaryStage; // Declare the Stage reference
 
-    @FXML
-    private Menu createnote;
-
-    @FXML
-    private Menu viewnote;
-
-    @FXML
-    private Menu takenote;
-
-    @FXML
-    private MenuBar optionmenu;
-
+    public void setPrimaryStage(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+    }
     @FXML
     private TableView<Categories> categorytab;
 
     @FXML
-    private TableColumn<Categories, String> categoryname;
+    private TableColumn<Categories, String> category_name;
 
     @FXML
-    private TableColumn<Categories, Integer> catid;
+    private TableColumn<Categories, Integer> id;
 
     @FXML
     private TableColumn<Categories, Date> created_date;
 
     public void initialize() {
-        catid.setCellValueFactory(new PropertyValueFactory<>("id"));
-        categoryname.setCellValueFactory(new PropertyValueFactory<>("category_name"));
+        id.setCellValueFactory(new PropertyValueFactory<>("id"));
+        category_name.setCellValueFactory(new PropertyValueFactory<>("category_name"));
         created_date.setCellValueFactory(new PropertyValueFactory<>("created_date"));
         viewCategories();
     }
@@ -62,19 +45,21 @@ public class ViewCategories {
         String user = "root";
         String pass = "";
 
-
         try{
             Class.forName("com.mysql.jdbc.Driver");
             Connection connection = DriverManager.getConnection(url,user,pass);
-            String sql = "Select * from category";
+
+            String sql = "SELECT * FROM category";
+
             PreparedStatement statement = connection.prepareStatement(sql);
             ResultSet resultSet = statement.executeQuery();
             ObservableList<Categories> categories = FXCollections.observableArrayList();
+
             while (resultSet.next()) {
-                int id = resultSet.getInt("id");
-                String category_name = resultSet.getString("category_name");
-                Date created_date = resultSet.getDate("created_date");
-                Categories category = new Categories();
+                int categoryId = resultSet.getInt("id");
+                String categoryName = resultSet.getString("category_name");
+                Date createdDate = resultSet.getDate("created_date");
+                Categories category = new Categories(categoryId, categoryName, createdDate);
                 categories.add(category);
             }
 
@@ -83,10 +68,9 @@ public class ViewCategories {
             resultSet.close();
             statement.close();
             connection.close();
-//            showAlert("Successfully login", "Login as "+u);
-        }catch(Exception e){
+        } catch(Exception e) {
             System.out.println("Connection failed.");
+            e.printStackTrace();
         }
     }
-
 }
