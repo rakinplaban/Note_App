275eb4a4f4ae72fc9ca877384ddd951dfc785f27
rakinplaban <rakinshahriar54@gmail.com>
2023-06-25 23:03:42 +0600

    Done with combobox.

--- src/main/java/com/example/noteapp/Takenote.java
+++ src/main/java/com/example/noteapp/Takenote.java
@@ -1,23 +1,20 @@
 package com.example.noteapp;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.control.MenuBar;
 import javafx.stage.Stage;
+import javafx.scene.layout.AnchorPane;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.io.IOException;
+import java.sql.*;
+
 public class Takenote {
     private Stage primaryStage; // Declare the Stage reference
 
@@ -25,7 +22,7 @@ public class Takenote {
         this.primaryStage = primaryStage;
     }
     @FXML
-    private ChoiceBox<?> category;
+    private ComboBox<String> category;
 
     @FXML
     private TextArea note;
@@ -36,10 +33,41 @@ public class Takenote {
     @FXML
     private TextField title;
 
+    @FXML
+    private AnchorPane rootAnchorPane;
+
     private int userId;
 
     public void setUserId(int userId) {
         this.userId = userId;
+    }
+
+    @FXML
+    public void rootAnchorPaneInitialized() {
+        try {
+            String url = "jdbc:mysql://localhost:3306/noteapp";
+            String user = "root";
+            String pass = "";
+
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(url, user, pass);
+            String sql = "SELECT category_name FROM category";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            ResultSet rs = statement.executeQuery();
+            ObservableList<String> data = FXCollections.observableArrayList();
+            while (rs.next()) {
+                data.add(rs.getString("category_name"));
+            }
+            category.setItems(data);
+        } catch (Exception e) {
+            e.printStackTrace();
+            showAlert("Error", "Failed to load categories.");
+        }
+    }
+
+    @FXML
+    private void initialize() {
+        rootAnchorPaneInitialized();
     }
 
     @FXML
@@ -58,6 +86,8 @@ public class Takenote {
             String note_text = note.getText();
             statement.setString(2,note_text);
             statement.setInt(3,userId);
+
+
             statement.executeUpdate();
 
             showAlert("Created Successful", "201 created!");
